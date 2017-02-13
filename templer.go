b35fb9ba7b952006c2511dba450c441e9a28805e
tasuten <heliopause64@gmail.com>
2017-02-13 19:52:16 +0900

    It may spawm a shell, but buggy

---
+++ templer.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"syscall"
+	"unsafe"
+
+	"./lib/login"
+)
+
+func main() {
+	original_term, _ := login.Tcgetattr(syscall.Stdin)
+	original_winsize := login.Winsize{}
+	_ = login.Ioctl(syscall.Stdin, syscall.TIOCGWINSZ, uintptr(unsafe.Pointer(&original_winsize)))
+
+	pid, master_fd, _ := login.Forkpty(*original_term, original_winsize)
+
+	switch pid {
+	case 0:
+		syscall.Exec("/bin/bash", []string{""}, []string{""})
+	default:
+		new_term, _ := login.Tcgetattr(syscall.Stdin)
+		login.Cfmakeraw(new_term)
+		login.Tcsetattr(syscall.Stdin, new_term)
+		go func() {
+			var buf = make([]byte, 256)
+
+			for {
+				nin, _ := syscall.Read(syscall.Stdin, buf)
+				if nin <= 0 {
+					break
+				}
+				syscall.Write(master_fd, buf[:nin])
+			}
+		}()
+
+		var buf = make([]byte, 256)
+
+		for {
+			nin, _ := syscall.Read(master_fd, buf)
+			if nin <= 0 {
+				break
+			}
+			syscall.Write(syscall.Stdout, buf[:nin])
+		}
+	}
+
+}
