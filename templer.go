0b4f1ca291be85bbfb5e00fbecb2d55a83674698
tasuten <heliopause64@gmail.com>
2017-02-18 00:23:37 +0900

    Ad-hoc goroutine-ized

--- templer.go
+++ templer.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"sync"
 	"syscall"
 
 	"./lib/pty"
@@ -13,23 +16,47 @@ func main() {
 
 	s, _ := shell.Spawn(original_term, winsize)
 
-	go func() {
-		var buf = make([]byte, 256)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go input_handler(&s, &wg)
+	go output_handler(&s, &wg)
+	wg.Wait()
 
-		for {
-			nin, _ := syscall.Read(syscall.Stdin, buf)
-			if nin < 0 {
-				break
-			}
-			if nin == 0 {
-			  continue
-			}
-			s.Write(buf[:nin])
-		}
-	}()
+	defer original_term.SetTermios(syscall.Stdin)
 
+}
+
+func input_handler(s *shell.Shell, wg *sync.WaitGroup) {
+	defer wg.Done()
 	var buf = make([]byte, 256)
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGCHLD)
+
+	for {
+		n_read, _ := syscall.Read(syscall.Stdin, buf)
+		if n_read < 0 {
+			break
+		}
+
+		if n_read == 0 {
+			select {
+			case <-c:
+				return
+			default:
+				continue
+			}
+
+		}
+
+		s.Write(buf[:n_read])
+	}
+
+}
+
+func output_handler(s *shell.Shell, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var buf = make([]byte, 256)
 	for {
 		data, err := s.Read(&buf)
 		if err != nil {
@@ -37,7 +64,4 @@ func main() {
 		}
 		syscall.Write(syscall.Stdout, data.Data)
 	}
-
-	defer original_term.SetTermios(syscall.Stdin)
-
 }
